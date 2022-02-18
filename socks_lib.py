4bb35d364bf8a46081e00e80936fdc497449d5a6
Yan <i29899014@gmail.com>
2022-02-18 15:27:13 +0300

    Refactor all clear bytes with struct module

--- socks_lib.py
+++ socks_lib.py
@@ -1,29 +1,32 @@
 import socket
+import struct
 import sys
 
 class SocksLib:
 
     def __init__(self, socks_ip, socks_port):
-        self.socks_ver = '\x05'
+        self.socks_ver = 5
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_address = (socks_ip, int(socks_port))
     
     def auth(self):
         self.sock.connect(self.server_address)
-        auth_msg = f'{self.socks_ver}\x01\x00'
-        self.sock.send(auth_msg.encode())
+        auth_msg = struct.pack('!BBBB', self.socks_ver, 2, 0, 2)
+        self.sock.send(auth_msg)
         answ = self.sock.recv(2)
 
-    def request(self, address, port):
+    def connect(self, address, port):
+        self.auth()
         try:
             self.target_addr = socket.inet_aton(address)
-            self.address_type = '\x01'
+            self.address_type = struct.pack('!B', 1)
         except socket.error:
             self.target_addr = address.encode()
-            self.address_type = '\x03' + chr(len(address))
-        self.target_port = chr(0)+chr(port)
-
-        request = f'{self.socks_ver}\x01\x00{self.address_type}'.encode() + self.target_addr + self.target_port.encode()
+            self.address_type = struct.pack("!BB", 3, len(address))
+        self.target_port = struct.pack('!H', port)
+        
+        request = struct.pack('!BBB', self.socks_ver, 1, 0) + self.address_type + self.target_addr + self.target_port
         self.sock.send(request)
-        answ = self.sock.recv(128)
+        answ = self.sock.recv(64)
+
         return self.sock
--- test_get.py
+++ test_get.py
@@ -1,9 +1,9 @@
 import socket
 from socks_lib import SocksLib as socks
+import sys
 
 a = socks('127.1', 1080)
-a.auth()
-socket = a.request('google.com', 80)
+socket = a.connect('google.com', 80)
 
 socket.send(b'GET / HTTP/1.1\r\nHost: google.com\r\n\r\n')
 data = socket.recv(4096)
