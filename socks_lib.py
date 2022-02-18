f8d87427a22deed924d97ba6d10853b6ee2ecabd
Yan <i29899014@gmail.com>
2022-02-18 16:06:28 +0300

    Add simple login:pass authentication

--- socks_lib.py
+++ socks_lib.py
@@ -9,14 +9,18 @@ class SocksLib:
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_address = (socks_ip, int(socks_port))
     
-    def auth(self):
+    def auth(self, login, password):
         self.sock.connect(self.server_address)
         auth_msg = struct.pack('!BBBB', self.socks_ver, 2, 0, 2)
         self.sock.send(auth_msg)
         answ = self.sock.recv(2)
+        
+        auth_msg = struct.pack("!BB", 1, len(login)) + login.encode() + struct.pack("!B", len(password)) + password.encode()
+        self.sock.send(auth_msg)
+        answ = self.sock.recv(2)
 
-    def connect(self, address, port):
-        self.auth()
+    def connect(self, address, port, login, password):
+        self.auth(login, password)
         try:
             self.target_addr = socket.inet_aton(address)
             self.address_type = struct.pack('!B', 1)
--- test_get.py
+++ test_get.py
@@ -1,9 +1,7 @@
-import socket
 from socks_lib import SocksLib as socks
-import sys
 
 a = socks('127.1', 1080)
-socket = a.connect('google.com', 80)
+socket = a.connect('google.com', 80, 'user', 'pass')
 
 socket.send(b'GET / HTTP/1.1\r\nHost: google.com\r\n\r\n')
 data = socket.recv(4096)
